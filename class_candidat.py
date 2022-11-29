de261993a86f1c352a7ba096cc170223a98d96a0
Nikita <405663260nikita@gmail.com>
2022-11-29 11:15:54 +0300

    add description to class_candidat.py and utils.py

--- class_candidat.py
+++ class_candidat.py
@@ -1,4 +1,7 @@
 class Candidate:
+    """
+    Класс кандидатов
+    """
     def __init__(self, id_, name, picture, position, gender, age, skills):
         self.id_ = id_
         self.name = name
@@ -9,24 +12,52 @@ class Candidate:
         self.skills = skills.lower().split(', ')
 
     def get_id(self):
+        """
+        Позволяет получить id кандидата
+        :return: id
+        """
         return self.id_
 
     def get_name(self):
+        """
+        Позволяет получить имя и фамилию кандидата
+        :return: name and surname
+        """
         return self.name
 
     def get_skills(self):
+        """
+        Позволяет получить список скиллов кандидата
+        :return: list with skills
+        """
         return self.skills
 
     def get_picture(self):
+        """
+        Позволяет получить ссылку на фото кандидата
+        :return: url
+        """
         return self.picture
 
     def get_position(self):
+        """
+        Позволяет получить должность кандидата
+        :return: position
+        """
         return self.position
 
     def get_gender(self):
+        """
+        Позволяет получить пол кандидата
+        :return: gender
+        """
         return self.gender
 
     def get_age(self):
+        """
+        Позволяет получить возраст кандидата
+        :return: age
+        """
         return self.age
 
     def __repr__(self):
--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ CANDIDATES_FILE_NAME = 'candidates.json'
 
 
 def load_candidates() -> dict:
+    """
+    Загружает кандидатов из json-файла, возвращает словарь с кандидатами
+    :return: dict
+    """
     with open(CANDIDATES_FILE_NAME, encoding='utf-8') as file:
         candidates_load = json.load(file)
         candidates = {str(candidate['id']): Candidate(*candidate.values()) for candidate in candidates_load}
@@ -14,11 +18,23 @@ def load_candidates() -> dict:
 
 
 def get_candidates_by_name(candidate_name: str, candidates: dict) -> list:
+    """
+    Находит кадидатов по имени
+    :param candidate_name: имя кандидата
+    :param candidates: словарь с кандидатами
+    :return: список кандидатов с именем
+    """
     needed_candidates = [candidate for candidate in candidates.values() if
                          candidate_name.capitalize() in candidate.get_name().split()]
     return needed_candidates
 
 
 def get_candidates_by_skill(skill_name: str, candidates: dict) -> list:
+    """
+    Находит кандидатов с определенными навыками
+    :param skill_name: скилл
+    :param candidates: словарь с кандидатами
+    :return: список кандидатов с этим скиллом
+    """
     needed_candidates = [candidate for candidate in candidates.values() if skill_name.lower() in candidate.get_skills()]
     return needed_candidates
